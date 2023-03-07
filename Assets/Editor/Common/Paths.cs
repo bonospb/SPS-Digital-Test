9bfa2efd027aa09a739bdd70f79776c2f18d2d76
Pavel Nikolaevskiy <pavel.nik@freeteam.ru>
2023-03-07 03:20:08 +0300

    [Added] editor commons

---
+++ Assets/Editor/Common/Paths.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace FreeTeam.BP.Editor.Common
+{
+    public static class Paths
+    {
+        #region Constants
+        public const string CONFIG_FILENAME = "config";
+        public static readonly string CONFIG_PATH = Path.Combine("Configs/", CONFIG_FILENAME);
+
+        public const string PROFILE_FILENAME = "profile";
+        public const string SETTINGS_FILENAME = "settings";
+        #endregion
+    }
+}
